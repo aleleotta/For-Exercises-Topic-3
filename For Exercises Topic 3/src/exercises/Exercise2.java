509fb2544672d1a4791978e62e204222d2710aa8
aleleotta <aleleotta@106-02.info2>
2022-11-10 10:17:52 +0100

    Exercise 2 concluded!

--- For Exercises Topic 3/src/exercises/Exercise2.java
+++ For Exercises Topic 3/src/exercises/Exercise2.java
@@ -1,10 +1,12 @@
 package exercises;
-import java.util.*;
 
 public class Exercise2 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		sc.close();
+		int i; //Increment
+		System.out.println("Printing even numbers from 0 to 200..."); //Message to user
+		for(i = 0; i <= 200; i = i + 2) { //1º Initialization, 2º Condition, 3º Increment
+			System.out.println(i); //Iteration print
+		}
+		System.out.println("Have a nice day!");
 	}
 }
-//Realiza un programa que muestre los números pares comprendidos entre el 1 y el 200. Para ello suma de 2 en 2.
