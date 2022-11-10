f5b59a2c6f3729235616cfbd3b6061d9f2a3b6cd
aleleotta <aleleotta@106-02.info2>
2022-11-10 09:55:59 +0100

    Exercise 1 initialized.

---
+++ For Exercises Topic 3/src/exercises/Exercise1.java
@@ -0,0 +1,10 @@
+package exercises;
+import java.util.*;
+
+public class Exercise1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sc.close();
+	}
+}
+/*Escribir una aplicación para aprender a contar, que pedirá un número n y mostrará todos los números del 1 al n.*/
